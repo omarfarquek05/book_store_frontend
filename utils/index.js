0dad4da5cead8c9507cb6a9be6d7378588985522
Omar Farquek Joy <89341479+omarfarquek05@users.noreply.github.com>
2024-02-18 23:28:45 +0600

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
+
 export const baseUrl ='https://book-store-backend-28eo.onrender.com/books';
 
-=======
-export const baseUrl = 'https://book-store-backend-28eo.onrender.com';
->>>>>>> ff64fbe48947a4bd5d083812de65d8dd4f626a87
